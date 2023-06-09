1487871a5343399ca3e64b36a74a308dc3c81231
Ellie <112726692+elliezub@users.noreply.github.com>
2023-06-09 14:36:20 -0400

    added dropdown function with JS

--- learning-cycles/main.js
+++ learning-cycles/main.js
@@ -1,5 +1,20 @@
-const dropbtn = document.querySelector(".dropdownbutton");
+// const dropbtn = document.querySelector(".dropdownbutton");
 const dropdiv = document.querySelector(".dropdowndiv");
+const dropdiv2 = document.querySelector('.dropdowndiv2');
+const dropdiv3 = document.querySelector('.dropdowndiv3');
 
+const dropbtn = document.querySelector('.dropdownbutton');
+const dropbtn2 = document.querySelector('.dropdownbutton2');
+const dropbtn3 =document.querySelector('.dropdownbutton3')
 
-// insert onclick function that reveals the dropdowndiv
+dropbtn.addEventListener('click', () => {
+    dropdiv.classList.toggle('active')
+})
+
+dropbtn2.addEventListener('click', () => {
+    dropdiv2.classList.toggle('active')
+})
+
+dropbtn3.addEventListener('click', () => {
+    dropdiv3.classList.toggle('active')
+})
