eebf1e6462b588dfb31c4185439016c5021a9025
elliezub <112726692+elliezub@users.noreply.github.com>
2023-06-13 16:32:35 -0300

    update dark mode

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
+const dropdivhtml = document.querySelector(".dropdivhtml");
+const contenthtml = document.querySelector(".exampledivcontenthtml");
 
-const dropdivhtml = document.querySelector('.dropdivhtml');
-const contenthtml = document.querySelector('.exampledivcontenthtml')
+contenthtml.addEventListener("click", () => {
+  dropdivhtml.classList.toggle("active");
+});
 
-contenthtml.addEventListener('click', () => {
-    dropdivhtml.classList.toggle('active');
-})
-
-const dropdivjs = document.querySelector('.dropdivjs');
+const dropdivjs = document.querySelector(".dropdivjs");
 const contentjs = document.querySelector(".exampledivcontentjs");
 
-contentjs.addEventListener('click', () => {
-    dropdivjs.classList.toggle('active');
-})
+contentjs.addEventListener("click", () => {
+  dropdivjs.classList.toggle("active");
+});
 
-const dropdivgpt =document.querySelector('.dropdivgpt');
+const dropdivgpt = document.querySelector(".dropdivgpt");
 const contentgpt = document.querySelector(".exampledivcontentgpt");
 
-contentgpt.addEventListener('click', () => {
-    dropdivgpt.classList.toggle('active');
-})
-
+contentgpt.addEventListener("click", () => {
+  dropdivgpt.classList.toggle("active");
+});
 
 //end of dropdown examples
 
 const toTop = document.querySelector("#totop");
 
-window.onscroll = () => toTop.style.display = window.scrollY > 20 ? 'block' : 'none';
+window.onscroll = () =>
+  (toTop.style.display = window.scrollY > 20 ? "block" : "none");
 
-toTop.onclick = () => window.scrollTo(0,0);
+toTop.onclick = () => window.scrollTo(0, 0);
+
+//end of to top button
+
+const darkModeToggle = document.querySelector('#darktoggle');
+
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+})
